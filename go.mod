f1d5453e0b983a89fab96b38f7375f55edd3e161
Dylan Murray <dylan@britevue.com>
2025-07-01 01:02:06 -0400

    Add go-tx-map source code with fixed linting

--- go.mod
+++ go.mod
@@ -2,10 +2,20 @@ module github.com/bsv-blockchain/go-tx-map
 
 go 1.24
 
-require github.com/stretchr/testify v1.10.0
+require (
+	github.com/dolthub/swiss v0.2.1
+	github.com/libsv/go-bt/v2 v2.0.0-00010101000000-000000000000
+	github.com/stretchr/testify v1.10.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dolthub/maphash v0.1.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+replace github.com/libsv/go-bt/v2 => github.com/ordishs/go-bt/v2 v2.2.22
---
+++ synced.go
@@ -0,0 +1,332 @@
+package txmap
+
+import (
+	"sync"
+
+	"github.com/dolthub/swiss"
+)
+
+// SyncedMap is a thread-safe generic map with read-write mutex synchronization.
+// It supports concurrent access and provides an optional item limit for constrained storage.
+type SyncedMap[K comparable, V any] struct {
+	mu    sync.RWMutex
+	m     map[K]V
+	limit int
+}
+
+// NewSyncedMap creates a new SyncedMap with an optional limit.
+// If the limit is set, the map will automatically delete a random item when the limit is reached.
+func NewSyncedMap[K comparable, V any](l ...int) *SyncedMap[K, V] {
+	limit := 0
+	if len(l) > 0 {
+		limit = l[0]
+	}
+
+	return &SyncedMap[K, V]{
+		m:     make(map[K]V),
+		limit: limit,
+	}
+}
+
+// Length returns the number of key-value pairs currently stored in the map. It is safe for concurrent access.
+func (m *SyncedMap[K, V]) Length() int {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	return len(m.m)
+}
+
+// Exists checks if a key exists in the map
+func (m *SyncedMap[K, V]) Exists(key K) bool {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	_, ok := m.m[key]
+
+	return ok
+}
+
+// Get returns a value in the map
+func (m *SyncedMap[K, V]) Get(key K) (V, bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	val, ok := m.m[key]
+
+	return val, ok
+}
+
+// Range returns a copy of the map.
+// Note that if v is a pointer, the map value may be modified by other goroutines at the same time.
+func (m *SyncedMap[K, V]) Range() map[K]V {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	items := make(map[K]V, len(m.m))
+
+	for k, v := range m.m {
+		items[k] = v
+	}
+
+	return items
+}
+
+// Keys returns a slice of keys in the map.
+func (m *SyncedMap[K, V]) Keys() []K {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	keys := make([]K, 0, len(m.m))
+
+	for k := range m.m {
+		keys = append(keys, k)
+	}
+
+	return keys
+}
+
+// Iterate iterates over the map and calls the function f for each key-value pair.
+// unlike Range(), Iterate does not return a copy of the map and keeps the lock for the duration of the iteration.
+// If f returns false, the iteration stops.
+func (m *SyncedMap[K, V]) Iterate(f func(key K, value V) bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	for k, v := range m.m {
+		if !f(k, v) {
+			return
+		}
+	}
+}
+
+// Set will set a key value pair in the map
+func (m *SyncedMap[K, V]) Set(key K, value V) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.setUnlocked(key, value)
+}
+
+// SetIfNotExists sets the value for the key if it does not already exist in the map.
+// It returns the existing value and false if the key already exists, or the new value and true if it was set.
+//
+// Params:
+//   - key: The key to set the value for.
+//   - value: The value to set for the key.
+//
+// Returns:
+//   - V: The value that was set or already existed.
+//   - bool: true if the value was set, false if the key already existed with a different value.
+func (m *SyncedMap[K, V]) SetIfNotExists(key K, value V) (V, bool) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if existingValue, ok := m.m[key]; ok {
+		return existingValue, false
+	}
+
+	m.setUnlocked(key, value)
+
+	return value, true
+}
+
+func (m *SyncedMap[K, V]) setUnlocked(key K, value V) {
+	if m.limit > 0 && len(m.m) >= m.limit {
+		for k := range m.m {
+			// delete a random item
+			delete(m.m, k)
+			break
+		}
+	}
+
+	m.m[key] = value
+}
+
+// SetMulti will set values for multiple keys
+func (m *SyncedMap[K, V]) SetMulti(keys []K, value V) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	// add the keys
+	for _, key := range keys {
+		m.setUnlocked(key, value)
+	}
+}
+
+// Delete deletes a key value from the map
+func (m *SyncedMap[K, V]) Delete(key K) bool {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	delete(m.m, key)
+
+	return true
+}
+
+// Clear empties the map
+func (m *SyncedMap[K, V]) Clear() bool {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.m = make(map[K]V)
+
+	return true
+}
+
+// SyncedSlice is a thread-safe wrapper around a slice of pointers, providing synchronized access and modification operations.
+type SyncedSlice[V any] struct {
+	mu    sync.RWMutex
+	items []*V
+}
+
+// NewSyncedSlice creates a new SyncedSlice.
+func NewSyncedSlice[V any](length ...int) *SyncedSlice[V] {
+	initialLength := 0
+	if len(length) > 0 {
+		initialLength = length[0]
+	}
+
+	return &SyncedSlice[V]{
+		items: make([]*V, 0, initialLength),
+	}
+}
+
+// Length returns the length of the slice
+func (s *SyncedSlice[V]) Length() int {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	return len(s.items)
+}
+
+// Size returns the size of the slice
+func (s *SyncedSlice[V]) Size() int {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	return cap(s.items)
+}
+
+// Get returns an item from the slice
+func (s *SyncedSlice[V]) Get(index int) (*V, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	if index < 0 || index >= len(s.items) {
+		return nil, false
+	}
+
+	return s.items[index], true
+}
+
+// Append will append an item to the slice
+func (s *SyncedSlice[V]) Append(item *V) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.items = append(s.items, item)
+}
+
+// Pop removes and returns the last item in the slice.
+func (s *SyncedSlice[V]) Pop() (*V, bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if len(s.items) == 0 {
+		return nil, false
+	}
+
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+
+	return item, true
+}
+
+// Shift removes and returns the first item in the slice.
+func (s *SyncedSlice[V]) Shift() (*V, bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if len(s.items) == 0 {
+		return nil, false
+	}
+
+	item := s.items[0]
+	s.items = s.items[1:]
+
+	return item, true
+}
+
+// SyncedSwissMap is a concurrent-safe wrapper around swiss.Map, providing locking mechanisms for thread-safety.
+type SyncedSwissMap[K comparable, V any] struct {
+	mu       sync.RWMutex
+	swissMap *swiss.Map[K, V]
+}
+
+// NewSyncedSwissMap returns a new synced map
+func NewSyncedSwissMap[K comparable, V any](length uint32) *SyncedSwissMap[K, V] {
+	return &SyncedSwissMap[K, V]{
+		swissMap: swiss.NewMap[K, V](length),
+	}
+}
+
+// Get returns a key value in the map
+func (m *SyncedSwissMap[K, V]) Get(key K) (V, bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	return m.swissMap.Get(key)
+}
+
+// Range gets the range of the map
+func (m *SyncedSwissMap[K, V]) Range() map[K]V {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	items := map[K]V{}
+
+	m.swissMap.Iter(func(key K, value V) bool {
+		items[key] = value
+		return false
+	})
+
+	return items
+}
+
+// Set sets a key value pair in the map
+func (m *SyncedSwissMap[K, V]) Set(key K, value V) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.swissMap.Put(key, value)
+}
+
+// Length returns the length of the map
+func (m *SyncedSwissMap[K, V]) Length() int {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	return m.swissMap.Count()
+}
+
+// Delete deletes a key value from the map
+func (m *SyncedSwissMap[K, V]) Delete(key K) bool {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	return m.swissMap.Delete(key)
+}
+
+// DeleteBatch batch deletes items in the map
+func (m *SyncedSwissMap[K, V]) DeleteBatch(keys []K) bool {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	ok := false
+
+	for _, key := range keys {
+		ok = m.swissMap.Delete(key)
+	}
+
+	return ok
+}
